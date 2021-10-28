c6ce0a37704008bb0d15acbe03afa10483ee08b3
Madeline Harrop <madeline.harrop@gmail.com>
2021-10-28 14:30:19 +0100

    Initial commit of files

---
+++ arrays/index.js
@@ -0,0 +1,12 @@
+const favouriteFilms = ["2012", "Hanna", "Deadpool", "Daylight",]
+console.log(favouriteFilms[0]);
+
+favouriteFilms.push("Volcano");
+
+console.table(favouriteFilms);
+
+favouriteFilms.slice()
+
+console.table(favouriteFilms);
+
+favouriteFilms.unshift("Alive");
